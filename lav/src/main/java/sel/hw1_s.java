44c74c44739454b6fd13af5e5ca472aa9926747d
lavo <linhuang1101@gmail.com>
2023-07-26 22:00:10 -0700

    update

---
+++ lav/src/main/java/sel/hw1_s.java
@@ -0,0 +1,7 @@
+package sel;
+
+public class hw1_s {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ lav/src/main/java/sel/hw2_s.java
@@ -0,0 +1,15 @@
+package sel;
+
+public class hw2_s {
+    public static void main(String[] args) {
+        String name = "Sam Vong";
+        int ages = 25;
+        String any;
+        any = "What Do you want ?";
+
+        System.out.println("First & Last " + ": " + name);
+        System.out.print("Ages" + ": " + ages);
+        System.out.println("Output" + ": " + any);
+
+    }
+}
